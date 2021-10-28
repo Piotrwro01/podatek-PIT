1b2daaf5bff1dbdb284cc2fae728f800228edb8c
PiotrWrotny <piotrwro01@gmail.com>
2021-10-28 23:33:25 +0200

    prosta struktura
    wstepny podzial na klasy obiekty oraz pierwszy wybor uzytkownika

--- podatek-pit/Program.cs
+++ podatek-pit/Program.cs
@@ -4,9 +4,34 @@ namespace podatek_pit
 {
     class Program
     {
+        static public void przedsiebiorca()
+        {
+
+        }
+
+        static public void czlowiek()
+        {
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Witaj w programie do obliczania skladki PIT. \n\nZadeklaruj swój status: \n   1. Osoba fizyczna. \n   2. Firma rozliczająca się na zasadach PIT");
+            Wybor: //punkt podjecia przez użytkownika decyzji kim jest, POCZĄTEK PROGRAMU
+            int wybor = int.Parse(Console.ReadLine());
+            switch (wybor)
+            {
+                case 1:
+                    przedsiebiorca();
+                    break;
+                case 2:
+                    czlowiek();
+                    break;
+                default:
+                    Console.Write("Niepoprawny wybór, spróbuj jeszcze raz: ");
+                    goto Wybor;
+
+            }
+
         }
     }
 }
---
+++ podatek-pit/PrzedsiebiorcaClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Przedsiebiorca
+{
+	public String nazwa;
+
+	public void zbierzDane()
+    {
+		Console.WriteLine("Jak nazywa się przedsiębiorstwo?");
+		nazwa = Console.ReadLine();
+    }
+
+	public void wyswietlNazwe()
+    {
+		Console.WriteLine("Przedsiebiorca " + nazwa);
+    }
+}
