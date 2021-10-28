dc917bcf6629905bbf70421a5de53cf4643aaa26
PiotrWrotny <piotrwro01@gmail.com>
2021-10-28 22:33:04 +0200

    poczatek projektu
    swiezy projekt zrobiony w vs

---
+++ podatek-pit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace podatek_pit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
